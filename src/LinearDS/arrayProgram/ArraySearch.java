c839171b71e9d8d920ad6275fbb0d14a3c886ff4
Lokesh3478 <loke22052.it@rmkec.ac.in>
2024-06-08 20:49:32 +0530

    Ceiling and Flooring in Binary Search

--- src/LinearDS/arrayProgram/ArraySearch.java
+++ src/LinearDS/arrayProgram/ArraySearch.java
@@ -1,4 +1,7 @@
 package LinearDS.arrayProgram;
+
+import java.util.Arrays;
+
 public class ArraySearch {
 	//TC O(n) SC O(1)
 	public int linearSearch(int arr[],int target) {
@@ -15,10 +18,29 @@ public class ArraySearch {
 		else if(arr[mid]>target) { return binarySearch(left, mid-1, arr,target); }
 		return binarySearch(mid+1, right, arr,target);
 	}
+	//Modified Binary Search returns largest number smaller than or equal to target 
+	public int floor(int left,int right,int arr[],int target) {
+		if(left>right) return right;
+		int mid = left + (right-left)/2;
+		if(arr[mid]==target) return mid;
+		else if(arr[mid]>target) { return floor(left, mid-1, arr,target); }
+		return floor(mid+1, right, arr,target);
+	}
+	//Modified Binary Search returns smallest number greater than or equal to target
+	public int ceil(int left,int right,int arr[],int target) {
+		if(left>right) return left;
+		int mid = left + (right-left)/2;
+		if(arr[mid]==target) return mid;
+		else if(arr[mid]>target) { return ceil(left, mid-1, arr,target); }
+		return ceil(mid+1, right, arr,target);
+	}
      public static void main(String[] args) {
-		int arr[] = new int[] {3,44,2221,443,11,42,66,2,5,1,5,2};
+		int arr[] = new int[] {1,3,6,13,45,67,77,89,90,234,566,771,891};
+		Arrays.sort(arr);
 		ArraySearch as = new ArraySearch();
 		System.out.println(as.linearSearch(arr, 44));
-		System.out.println(as.binarySearch(0, arr.length, arr, 44));
+		System.out.println(as.binarySearch(0, arr.length-1, arr, 44));
+		System.out.println(as.ceil(0, arr.length-1, arr, 100));
+		System.out.println(as.floor(0, arr.length-1, arr, 100));
 	}
 }
