0b07445be5c06c51408c294a10227bd93ceb0dd6
Lokesh3478 <loke22052.it@rmkec.ac.in>
2024-06-20 21:20:26 +0530

    cyclic sort problems added

--- src/LinearDS/arrayProgram/sorting/CyclicSort1toN.java
+++ src/LinearDS/arrayProgram/sorting/CyclicSort1toN.java
@@ -127,6 +127,35 @@ public class CyclicSort1toN {
         return new int[]{-1};
     }
 	
+	
+	//LeetCode 41 First Missing Positive
+	
+	public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+        int i =0;
+        // Place each number in its correct position if possible
+        while(i<n){
+        	//Ignore duplicates , number out of bound between 1 to array.length 
+        	//Swap till correct index 
+            while(nums[i]>0&&nums[i]<=n&&nums[i]-1!=i&&nums[nums[i]-1]!=nums[i]){
+                int temp = nums[nums[i]-1];
+                nums[nums[i]-1] = nums[i];
+                nums[i] = temp;
+            }
+            i++;
+        }
+        // Find the first position where the index + 1 doesn't match the value
+        for (i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        
+        // If all positions are correct, the missing number is n + 1
+        return n + 1;
+    }
+	
+	
 	public static void main(String[] args) {
 		int[][] testCases = {
 	            {3, 1, 2},
